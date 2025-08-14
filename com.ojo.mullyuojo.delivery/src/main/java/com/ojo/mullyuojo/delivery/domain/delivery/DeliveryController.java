6f6ca7c63a90560b7be4219132147dd3061390eb
pigglehyun <sarah0661@pusan.ac.kr>
2025-08-14 20:37:16 +0900

    Feat(D): add basic CRUD (미완)

---
+++ com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/delivery/DeliveryController.java
@@ -0,0 +1,43 @@
+package com.ojo.mullyuojo.delivery.domain.delivery;
+
+
+import com.ojo.mullyuojo.delivery.domain.com_delivery.CompanyDeliveryService;
+import com.ojo.mullyuojo.delivery.domain.delivery.dto.DeliveryRequestDto;
+import com.ojo.mullyuojo.delivery.domain.delivery.dto.DeliveryResponseDto;
+import com.ojo.mullyuojo.delivery.domain.hub_delivery.HubDeliveryService;
+import com.ojo.mullyuojo.delivery.utils.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/delivery")
+public class DeliveryController {
+
+    private final DeliveryService deliveryService;
+
+
+    @GetMapping()
+    public ApiResponse<?> getAllDelivery(){
+        List<DeliveryResponseDto> response = deliveryService.getAllDelivery();
+        return ApiResponse.success(200, response);
+    }
+
+    //주문생성시 자동으로 호출 -> message queue 활용
+    @PostMapping()
+    public ApiResponse<?> createDelivery(@RequestBody @Valid DeliveryRequestDto requestDto){
+        deliveryService.createDelivery(requestDto);
+        return ApiResponse.success(201, "배송 생성 완료");
+    }
+
+    @DeleteMapping("/{delivery_id}")
+    public ApiResponse<?> deleteDelivery(@PathVariable(name = "delivery_id") Long delivery_id){
+        deliveryService.deleteDelivery(delivery_id);
+        return ApiResponse.success(200, "배송 삭제 완료");
+    }
+
+
+}
---
+++ com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/delivery/DeliveryService.java
@@ -0,0 +1,115 @@
+package com.ojo.mullyuojo.delivery.domain.delivery;
+
+import com.ojo.mullyuojo.delivery.domain.com_delivery.CompanyDeliveryService;
+import com.ojo.mullyuojo.delivery.domain.delivery.client.DeliveryUserDto;
+import com.ojo.mullyuojo.delivery.domain.delivery.dto.DeliveryRequestDto;
+import com.ojo.mullyuojo.delivery.domain.delivery.dto.DeliveryResponseDto;
+import com.ojo.mullyuojo.delivery.domain.delivery.status.DeliveryStatus;
+import com.ojo.mullyuojo.delivery.domain.hub_delivery.HubDeliveryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+    private final HubDeliveryService hubDeliveryService;
+    private final CompanyDeliveryService companyDeliveryService;
+    private final DeliveryUserDto user = new DeliveryUserDto(1L, "MASTER");
+
+    public List<DeliveryResponseDto> getAllDelivery() {
+
+
+
+        //Master
+        List<Delivery> deliveryList = deliveryRepository.findAll();
+
+        //Hub Manager
+        //List<Delivery> deliveryList = deliveryRepository.findAllByHubManagerId();
+
+        //Delivery Manager
+        //List<Delivery> deliveryList = deliveryRepository.findAllByCompanyDeliveryManagerId();
+
+        return deliveryList.stream()
+                .map(DeliveryResponseDto::from)
+                .toList();
+    }
+
+    public DeliveryResponseDto getDelivery(Long id) {
+
+
+        Delivery delivery = findById(id);
+//        //허브관리자
+//        String role = "HUB";
+//        if ( role.equals("HUB")) {
+//            //배송의 출발 허브 ID & 도착 허브 ID를 통해 허브 객체 가져오기 ( HUB FeignClient )
+//            //해당 허브의 허브 담당자 id 가져오기
+//            //본인 허브인지 확인
+//        }
+//        //허브배송관리자 ---> 소속된 허브가 없는디?...
+//        String role2 = "HUB_DELIVERY";
+//        if ( role2.equals("DELIVERY")) {
+//            //배송담당자_허브배송담당자 테이블에서 허브ID 가져오기 ( DeliveryUser FeignClient)
+//            //배송의 출발허브 ID & 도착 허브 ID와 비교
+//            //본인 허브인지 확인
+//        }
+        //업체 배송 관리자
+//        String role3 = "COM_DELIVERY";
+//        if ( role2.equals("COM_DELIVERY")) {
+//            //배송의 업체 배송 담당자와 비교
+//            //본인 담당인지 확인
+//        }
+//
+
+        return DeliveryResponseDto.from(delivery);
+    }
+
+    public void createDelivery(DeliveryRequestDto requestDto) {
+        Delivery delivery = new Delivery(requestDto.orderId(),
+                requestDto.deliveryStatus(),
+                requestDto.originHubId(),
+                requestDto.destinationHubId(),
+                requestDto.companyAddress(),
+                requestDto.companyManagerId(),
+                requestDto.companyManagerSlackId(),
+                requestDto.companyDeliveryManagerId());
+
+        hubDeliveryService.createHubDeliveryByDelivery(delivery);
+        companyDeliveryService.createCompanyDeliveryByDelivery(delivery);
+    }
+
+    public void changeStatus(Long id, DeliveryStatus status) {
+
+        Delivery delivery = findById(id);
+        if (status.equals(DeliveryStatus.IN_TRANSIT_TO_HUB)){
+            delivery.changeStatus(DeliveryStatus.IN_TRANSIT_TO_HUB);
+        } else if(status.equals(DeliveryStatus.ARRIVED_AT_DEST_HUB)){
+            delivery.changeStatus(DeliveryStatus.ARRIVED_AT_DEST_HUB);
+        } else if(status.equals(DeliveryStatus.OUT_FOR_DELIVERY)){
+            delivery.changeStatus(DeliveryStatus.OUT_FOR_DELIVERY);
+        } else if(status.equals(DeliveryStatus.IN_TRANSIT_TO_COMPANY)){
+            delivery.changeStatus(DeliveryStatus.IN_TRANSIT_TO_COMPANY);
+        } else if(status.equals(DeliveryStatus.DELIVERED)){
+            delivery.changeStatus(DeliveryStatus.DELIVERED);
+        }
+    }
+
+    public void deleteDelivery(Long id) {
+        Long userId = user.getUserId();
+        Delivery delivery = findById(id);
+        delivery.softDelete(userId);
+    }
+
+    private Delivery findById(Long id){
+        Delivery delivery = deliveryRepository.findById(id).orElseThrow(() ->  new RuntimeException("해당 배송을 찾을 수 없습니다."));
+        if ( delivery.getDeletedBy() == null) { return delivery; }
+        else { throw new RuntimeException("삭제된 배송은 접근할 수 없습니다.");}
+    }
+
+
+}
