54c5ea22d668b2ed8d5753e4c317d4c4341376e6
pigglehyun <sarah0661@pusan.ac.kr>
2025-08-18 19:20:44 +0900

    Feat(CD): add CompanyDelivery CRUD logic

--- com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/com_delivery/CompanyDeliveryRepository.java
+++ com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/com_delivery/CompanyDeliveryRepository.java
@@ -1,8 +1,24 @@
 package com.ojo.mullyuojo.delivery.domain.com_delivery;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CompanyDeliveryRepository extends JpaRepository<CompanyDelivery,Long> {
+public interface CompanyDeliveryRepository extends JpaRepository<CompanyDelivery, Long> {
+    CompanyDelivery findByDeliveryIdAndDeletedByIsNull(Long deliveryId);
+
+    Optional<CompanyDelivery> findByIdAndDeletedByIsNull(Long id);
+
+    @Query("select d from com_delivery_tb d where d.originHubId in :hubIds and d.deletedBy = null")
+    List<CompanyDelivery> findAllByHubIds(@Param("hubIds") List<Long> hubIdList);
+
+    List<CompanyDelivery> findAllByCompanyDeliveryManagerIdAndDeletedByIsNull(Long companyDeliveryManagerId);
+
+    @Query("select d from com_delivery_tb d where d.destinationCompanyId in :companyIdList and d.deletedBy = null")
+    List<CompanyDelivery> findAllByDestinationCompanyIds(@Param("companyIdList") List<Long> companyIdList);
 }
--- com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/hub_delivery/HubDeliveryController.java
+++ com.ojo.mullyuojo.delivery/src/main/java/com/ojo/mullyuojo/delivery/domain/hub_delivery/HubDeliveryController.java
@@ -1,9 +1,37 @@
 package com.ojo.mullyuojo.delivery.domain.hub_delivery;
 
+import com.ojo.mullyuojo.delivery.domain.hub_delivery.dto.HubDeliveryResponseDto;
+import com.ojo.mullyuojo.delivery.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/hub-delivery-channel")
 public class HubDeliveryController {
+
+    private final HubDeliveryService hubDeliveryService;
+
+    @GetMapping()
+    public ApiResponse<?> getAllHubDelivery() {
+        List<HubDeliveryResponseDto> response = hubDeliveryService.getAllHubDelivery();
+        return ApiResponse.success(200, response);
+    }
+
+    @GetMapping("/{hubDeliveryId}")
+    public ApiResponse<?> getHubDelivery(@PathVariable(name = "hubDeliveryId") Long hubDeliveryId) {
+        HubDeliveryResponseDto response = hubDeliveryService.getHubDelivery(hubDeliveryId);
+        return ApiResponse.success(200, response);
+    }
+
+    public ApiResponse<?> deleteHubDelivery(@PathVariable(name = "hubDeliveryId") Long hubDeliveryId) {
+        hubDeliveryService.deleteHubDelivery(hubDeliveryId);
+        return ApiResponse.success(200, "배송 삭제 완료");
+    }
+
 }
