1afc5906979460c428655e19b8cee7497b88cf1a
dagyeom <105848447+ktkt1413@users.noreply.github.com>
2025-08-21 20:37:42 +0900

    Delete company directory

--- company/src/main/java/com/ojo/mullyuojo/company/application/security/Action.java
+++
@@ -1,6 +0,0 @@
-package com.ojo.mullyuojo.company.application.security;
-
-public enum Action {
-    CREATE, UPDATE, DELETE, READ
-}
-
--- company/src/main/java/com/ojo/mullyuojo/company/application/security/Role.java
+++
@@ -1,8 +0,0 @@
-package com.ojo.mullyuojo.company.application.security;
-
-public enum Role {
-    MASTER,
-    HUB_MANAGER,
-    DELIVERY_USER,
-    COMPANY_STAFF
-}
