ad705a6da2bf6768a48205b3e3c394932b8a9542
Stock05 <162933999+Stock05@users.noreply.github.com>
2025-08-22 19:18:21 +0900

    Delete user directory

--- user/src/main/java/com/ojo/mullyuojo/domain/user/enums/UserRole.java
+++
@@ -1,8 +0,0 @@
-package com.ojo.mullyuojo.domain.user.enums;
-
-public class UserRole {
-    public enum UserRoles {
-        MASTER, HUB_MANAGER, COMPANY_MANAGER, HUB_DELIVERY_MANAGER, COMPANY_DELIVERY_MANAGER
-
-    }
-}
