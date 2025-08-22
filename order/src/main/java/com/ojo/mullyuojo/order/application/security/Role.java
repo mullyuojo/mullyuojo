9e237d281cc1ccd1a2a62e33385af2a25b55b3de
Stock05 <162933999+Stock05@users.noreply.github.com>
2025-08-22 19:51:31 +0900

    Delete order directory

--- order/src/main/java/com/ojo/mullyuojo/order/application/security/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ojo.mullyuojo.order.application.security;
-
-public enum Role {
-    MASTER, HUB_MANAGER, COMPANY_MANAGER, HUB_DELIVERY_MANAGER, COMPANY_DELIVERY_MANAGER
-}
--- order/src/main/java/com/ojo/mullyuojo/order/hub/src/main/java/com/ojo/mullyuojo/hub/application/security/Role.java
+++
@@ -1,5 +0,0 @@
-package com.ojo.mullyuojo.hub.application.security;
-
-public enum Role {
-    MASTER, HUB_MANAGER, COMPANY_MANAGER, HUB_DELIVERY_MANAGER, COMPANY_DELIVERY_MANAGER
-}
